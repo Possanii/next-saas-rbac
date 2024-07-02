1b145dd6f917e1ba9fe39d8e956b547257810f6e
Possanii <gpossanii@outlook.com>
2024-07-01 22:16:17 -0300

    feat: configure fastify

--- apps/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineAbilityFor, projectSchema } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
-
-const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
-
-console.log(ability.can('delete', project))
