88729249eba9eaf89d423a011818c4bb26c669fc
Possanii <gpossanii@outlook.com>
2024-07-18 11:28:23 -0300

    feat: create organization route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -17,6 +17,7 @@ import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { env } from '@saas/env'
+import { createOrganization } from './routes/orgs/create-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -61,6 +62,9 @@ app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
 app.register(authenticateWithGithub)
+
+// organizations
+app.register(createOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('🔥 HTTP server running')
