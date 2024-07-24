601ae2d4dc7eaf05938c08a56302d328ceba0379
Possanii <gpossanii@outlook.com>
2024-07-24 12:56:32 -0300

    feat: delete project route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -24,6 +24,7 @@ import { getOrganizations } from './routes/orgs/get-organizations'
 import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { createProject } from './routes/projects/create-project'
+import { deleteProject } from './routes/projects/delete-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -80,6 +81,7 @@ app.register(transferOrganization)
 
 // projects
 app.register(createProject)
+app.register(deleteProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('🔥 HTTP server running')
