ca392e97277cd34db4fdf595f632bfb94868ff48
Possanii <gpossanii@outlook.com>
2024-07-24 11:51:08 -0300

    refactor: remove unused imports

--- apps/api/src/http/routes/orgs/create-organization.ts
+++ apps/api/src/http/routes/orgs/create-organization.ts
@@ -1,11 +1,10 @@
 import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
+import { createSlug } from '@/utils/create-slug'
 import type { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
-import { organizationSchema } from '../../../../../../packages/auth/src'
 import { BadRequestError } from '../_errors/bad-request-error'
-import { createSlug } from '@/utils/create-slug'
 
 export async function createOrganization(app: FastifyInstance) {
   app
