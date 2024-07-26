0c31610c9fdaa5466151bd18f447b91d01399a0b
Possanii <gpossanii@outlook.com>
2024-07-26 09:47:24 -0300

    feat: change to get

--- apps/api/src/http/routes/invites/get-invite.ts
+++ apps/api/src/http/routes/invites/get-invite.ts
@@ -6,7 +6,7 @@ import z from 'zod'
 import { BadRequestError } from '../_errors/bad-request-error'
 
 export async function getInvite(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().post(
+  app.withTypeProvider<ZodTypeProvider>().get(
     '/invites/:inviteId',
     {
       schema: {
