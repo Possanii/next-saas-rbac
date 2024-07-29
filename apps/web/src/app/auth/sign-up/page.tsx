fd04d17c67262fcda44ff05bac99eb9e32315c9e
Possanii <gpossanii@outlook.com>
2024-07-29 14:21:50 -0300

    feat: sign-up page

---
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -0,0 +1,69 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import githubIcon from '@/assets/github-icon.svg'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+export default function SignUpPage() {
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-xl">Sign Up</CardTitle>
+        <CardDescription>
+          Enter your information to create an account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" placeholder="Max" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password-confirmation">Confirm your password</Label>
+            <Input id="password-confirmation" type="password" />
+          </div>
+          <Button type="submit" className="w-full">
+            Create an account
+          </Button>
+          <Button variant="outline" className="w-full">
+            <Image
+              src={githubIcon}
+              className="mr-2 size-4 dark:invert"
+              alt=""
+            />
+            Sign up with Github
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Already have an account?{' '}
+          <Link href="/auth/sign-in" className="underline">
+            Sign in
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
