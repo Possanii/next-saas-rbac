5dc52cac7b859063ecf796500958b16992cad3ae
Possanii <gpossanii@outlook.com>
2024-07-30 12:05:59 -0300

    feat: sign-in with github

---
+++ apps/web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,56 @@
+import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+import { signInWithGithub } from '@/http/sign-in-with-github'
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+
+  const code = searchParams.get('code')
+
+  if (!code) {
+    return NextResponse.json(
+      {
+        message: 'Github OAuth code was not found.',
+      },
+      { status: 400 },
+    )
+  }
+
+  try {
+    const { token } = await signInWithGithub({ code })
+
+    cookies().set('saas:token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
+
+    const redirectURL = request.nextUrl.clone()
+
+    redirectURL.pathname = '/'
+    redirectURL.search = ''
+
+    return NextResponse.redirect(redirectURL)
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json<{ message: string }>()
+
+      return NextResponse.json(
+        {
+          message,
+        },
+        { status: 422 },
+      )
+    }
+
+    console.error(err)
+
+    const redirectURL = request.nextUrl.clone()
+
+    redirectURL.pathname = '/'
+    redirectURL.search = ''
+
+    return NextResponse.redirect(redirectURL + '/auth/sign-in')
+  }
+}
--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -12,6 +12,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { useFormState } from '@/hooks/use-form-state'
 
+import { signInWithGithub } from '../actions'
 import { signInWithEmailAndPassword } from './actions'
 
 export default function SignInForm() {
@@ -24,51 +25,60 @@ export default function SignInForm() {
     },
   })
   return (
-    <form onSubmit={handleSubmit} className="grid gap-4">
-      {success === false && message && (
-        <Alert variant="destructive">
-          <AlertTriangle className="size-4" />
-          <AlertTitle>Sign In failed!</AlertTitle>
-          <AlertDescription>{message}</AlertDescription>
-        </Alert>
-      )}
+    <div className="grid gap-4">
+      <form onSubmit={handleSubmit} className="grid gap-4">
+        {success === false && message && (
+          <Alert variant="destructive">
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Sign In failed!</AlertTitle>
+            <AlertDescription>{message}</AlertDescription>
+          </Alert>
+        )}
 
-      <div className="grid gap-2">
-        <Label htmlFor="email">Email</Label>
-        <Input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="m@example.com"
-          required
-          errors={errors?.email && errors.email[0]}
-        />
-      </div>
-      <div className="grid gap-2">
-        <div className="flex items-center">
-          <Label htmlFor="password">Password</Label>
-          <Link
-            href="/auth/forgot-password"
-            className="ml-auto inline-block text-sm underline"
-          >
-            Forgot your password?
-          </Link>
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="m@example.com"
+            required
+            errors={errors?.email && errors.email[0]}
+          />
         </div>
-        <Input
-          id="password"
-          name="password"
-          type="password"
-          required
-          errors={errors?.password && errors.password[0]}
-        />
-      </div>
-      <Button type="submit" className="w-full" disabled={isPending}>
-        {isPending ? <Loader2 className="size-4 animate-spin" /> : 'Login'}
-      </Button>
-      <Button variant="outline" className="w-full">
-        <Image src={githubIcon} className="mr-2 size-4 dark:invert" alt="" />
-        Login with Github
-      </Button>
-    </form>
+        <div className="grid gap-2">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+            <Link
+              href="/auth/forgot-password"
+              className="ml-auto inline-block text-sm underline"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            errors={errors?.password && errors.password[0]}
+          />
+        </div>
+        <Button type="submit" className="w-full" disabled={isPending}>
+          {isPending ? <Loader2 className="size-4 animate-spin" /> : 'Login'}
+        </Button>
+      </form>
+      <form action={signInWithGithub}>
+        <Button
+          type="submit"
+          variant="outline"
+          formAction={signInWithGithub}
+          className="w-full"
+        >
+          <Image src={githubIcon} className="mr-2 size-4 dark:invert" alt="" />
+          Login with Github
+        </Button>
+      </form>
+    </div>
   )
 }
