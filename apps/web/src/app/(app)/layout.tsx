336aca6014fe96a51307b66fef628dd9c26c21bf
Possanii <gpossanii@outlook.com>
2024-08-04 12:45:36 -0300

    fix: scroll bar

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout({
   return (
     <div className="flex min-h-screen w-full flex-col">
       {dynamicComponents}
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 px-4 md:gap-8 md:px-10">
         <div className="mx-auto w-full max-w-screen-2xl">{children}</div>
       </main>
     </div>
